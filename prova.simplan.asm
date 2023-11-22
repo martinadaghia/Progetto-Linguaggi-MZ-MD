6d7f4f42fbc2a759bee1090433d3f810dd7abc27
martinadaghia10 <martina.daghia@gmail.com>
2023-11-22 10:56:00 +0100

    modifiche varie dopo diversi test

--- prova.simplan.asm
+++ prova.simplan.asm
@@ -1,21 +1,19 @@
 //start ProgNode
-//start GreatNode
+//start EqualNode
 //start IntNode
-storei A0 5
+storei A0 3
 //end IntNode
 pushr A0 
-//start IntNode
-storei A0 7
-//end IntNode
+//start BoolNode
+storei A0 0
+//and BoolNode
 popr T1 
-bleq T1 A0 label0 
-label2: 
-storei A0 1 
-b label1 
-label0: 
-beq T1 A0 label2 
-storei A0 0 
-label1: 
-//end GreatNode
+beq A0 T1 label0
+storei A0 0
+b label1
+label0:
+storei A0 1
+label1:
+//and EqualNode
 halt
 //end ProgNode
